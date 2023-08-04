62e353898b68c558fa242baf0eea99914ea894d5
ruizrick7 <140860781+ruizrick7@users.noreply.github.com>
2023-08-04 00:18:39 -0400

    Add files via upload

---
+++ house.py
@@ -0,0 +1,11 @@
+# Compares multiple strings with if/elif/else
+
+name = input("What's your name? ")
+
+match name:
+    case "Harry" | "Hermoine" | "Ron":
+        print("Gryffindor")
+    case "Draco":
+        print("Slytherin")
+    case _:
+        print("Who")
